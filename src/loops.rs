707a4e550e580255270fee64b75962a5012f9aa8
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 14:10:14 -0600

    Successful FizzBuzz

--- src/loops.rs
+++ src/loops.rs
@@ -1,10 +1,46 @@
 /*
-
+    It is possible to create infinite loops in Rust.
 
 */
 
 pub fn run() {
 
+    let mut count = 0;
 
+    // loop {
+        
+    //     count += 1;
 
+    //     println!("No.: {}", count);
+
+    //     if count == 20 {
+    //         break;
+    //     }
+
+    // }
+
+    // While Loop
+    while count <= 100 {
+
+        if count % 15 == 0 {
+
+            println!("FizzBuzz");
+
+        } else if count % 3 == 0 {
+            
+            println!("Fizz");
+
+        } else if count % 5 == 0 {
+            
+            println!("Buzz");
+
+        } else {
+
+            println!("{}", count);
+
+        }
+
+        count += 1;
+        
+    }
 }
