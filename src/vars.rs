2db34a8882bb5f9e797f837fb8c093b74f665b91
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 06:30:03 -0600

    Deeper into variables...

--- src/vars.rs
+++ src/vars.rs
@@ -5,8 +5,23 @@
 pub fn run() {
 
     let name = "Aaron";
+    let mut age  = 37;  // `mut` allows us to change the value of
+                        // `age` downstream of its original
+                        // initialization. Its use here wil cause
+                        // the compiler to throw up a warning,
+                        // because the value of `age` is not
+                        // modified after its original init.
+                        // Yet, the code will compile and run. 
 
-    println!("My name is {}.", name);
+    println!("My name is {} and my age is {}.", name, age);
+
+    // Defining CONSTANTS
+    const ID: u32 = 001;
+    println!("ID: {}.", ID);
+
+    // Assigning multiple variables inline
+    let ( my_name, my_age ) = ("Aaron",37);
+    println!("{} is {}.", my_name, my_age);
 
 }
  
