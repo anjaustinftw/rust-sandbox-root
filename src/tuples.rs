4ea375ecae97c193dfe4abf391af598983000e4f
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 09:57:43 -0600

    Tuples End

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,8 +1,11 @@
 /*
-
-
+    Tuples group values of different types.
+    Limitation: Max of 12 elements
 */
 
 pub fn run() {
+    let person: (&str, &str, i8) = ("Aaron", "Colorado", 48);
 
+    println!("{} is from {} and his age is {}.", person.0, person.1, person.2);
+    
 }
