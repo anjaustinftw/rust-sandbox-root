1f08938379f08541c8049c11bbdd25aad39bc61c
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 09:10:25 -0600

    Pushing chars

--- src/strings.rs
+++ src/strings.rs
@@ -7,11 +7,18 @@
 pub fn run() {
 
     // str data type inferred
-    let str_hello = "Hello";
+    let str_hello = "Hello, ";
+
+    /*
+        str_hello.push('W');
+        Will result in: error[E0599]: no method named `push` found for reference `&str` in the current scope
+    */ 
 
     // String data type explicit
-    let string_hello = String::from("hello!");
+    let mut string_hello = String::from("hello! ");
 
-    println!("{}, {} {strl} by {stringl}.", str_hello, string_hello, strl = str_hello.len(), stringl = string_hello.len());
+    string_hello.push('\u{1F600}'); // ...will succeed, when string_hello is set to `mut`. Can only ad hoc one `char`.
+
+    println!("{}{}\n{strl}:{stringl}", str_hello, string_hello, strl = str_hello.len(), stringl = string_hello.len());
 
 }
