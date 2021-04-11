9cb21faff081e5a0557fb4958191b8e483ea77ec
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 08:40:06 -0600

    Updated types.rs comments and formatting

--- src/types.rs
+++ src/types.rs
@@ -1,30 +1,39 @@
-// Data Types in Rust : https://doc.rust-lang.org/book/ch03-02-data-types.html#:~:text=Rust%20has%20two%20primitive%20compound%20types%3A%20tuples%20and%20arrays.
-// bits & bytes
-// bool : The boolean type.
-// char : A character type.
-// int
-// i8 : The 8-bit signed integer type.
-// i16 : The 16-bit signed integer type.
-// i32 : The 32-bit signed integer type.
-// i64 : The 64-bit signed integer type.
-// isize : The pointer-sized signed integer type.
-// uint
-// u8 : The 8-bit unsigned integer type.
-// u16 : The 16-bit unsigned integer type.
-// u32 : The 32-bit unsigned integer type.
-// u64 : The 64-bit unsigned integer type.
-// usize : The pointer-sized unsigned integer type.
-// float
-// f32 : The 32-bit floating point type.
-// f64 : The 64-bit floating point type.
-// Compound Data Types
-// array : A fixed-size array, denoted [T; N], for the element type, T, and the non-negative compile-time constant size, N.
-// slice : A dynamically-sized view into a contiguous sequence, [T].
-// str : String slices.
-// tuple : A finite heterogeneous sequence, (T, U, ..).
+/*
+    Data Types in Rust
+    SOURCE: https://doc.rust-lang.org/book/ch03-02-data-types.html#:~:text=Rust%20has%20two%20primitive%20compound%20types%3A%20tuples%20and%20arrays.
+    
+    bits & bytes
+    bool : The boolean type.
+    char : A character type.
+    
+    int
+    i8 : The 8-bit signed integer type.
+    i16 : The 16-bit signed integer type.
+    i32 : The 32-bit signed integer type.
+    i64 : The 64-bit signed integer type.
+    isize : The pointer-sized signed integer type.
+    
+    uint
+    u8 : The 8-bit unsigned integer type.
+    u16 : The 16-bit unsigned integer type.
+    u32 : The 32-bit unsigned integer type.
+    u64 : The 64-bit unsigned integer type.
+    usize : The pointer-sized unsigned integer type.
+    
+    float
+    f32 : The 32-bit floating point type.
+    f64 : The 64-bit floating point type.
+    
+    Compound Data Types
+    array : A fixed-size array, denoted [T; N], for the element type, T, and the non-negative compile-time constant size, N.
+    slice : A dynamically-sized view into a contiguous sequence, [T].
+    str : String slices.
+    tuple : A finite heterogeneous sequence, (T, U, ..).
 
-// Rust is a statically typed language, but it does have the ability to infer types.
-// Do the work for the compiler when you can.
+    Rust is a statically typed language, but it does have the ability to infer types.
+    Do the work for the compiler when you can.
+
+*/
 
 pub fn run() {
 
@@ -57,20 +66,23 @@ pub fn run() {
 
     println!("{:?}", (x, y, z, is_active, is_greater, a1a, face));
 
+    // I figured out how to pass a tuple.
     print_type_of_tuple(&(x, y, z, is_active, is_greater, a1a, face));
 
-    // Function to print variable type
-    // SOURCE: https://stackoverflow.com/questions/21747136/how-do-i-print-the-type-of-a-variable-in-rust
-    // 
-    // fn print_type_of<T>(_: &T) {
-    //     println!("{}", std::any::type_name::<T>())
-    // }
-
-    // I figured out how to pass a tuple
-    // print_type_of_tuple(&(x, y, z, is_active, is_greater, a1a, face));
-    //
     fn print_type_of_tuple<T>(_: &T) {
         println!("{:?}", std::any::type_name::<T>())
     }
 
 }
+
+    /*
+        I wanted the ability to print variable types.
+        
+        Function to print variable types in Rust
+        SOURCE: https://stackoverflow.com/questions/21747136/how-do-i-print-the-type-of-a-variable-in-rust
+        
+        fn print_type_of<T>(_: &T) {
+            println!("{}", std::any::type_name::<T>())
+        }
+
+    */
