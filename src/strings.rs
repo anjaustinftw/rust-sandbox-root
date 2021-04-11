ce8c5d212a6b8bad8478bd11c91d64ecc3dca010
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 08:59:34 -0600

    Prototyping

--- src/strings.rs
+++ src/strings.rs
@@ -1,9 +1,17 @@
 /*
-
-
-
+    String Variants [Primitive str, String]
+    str = immutable, fixed-length string stored in memory
+    String = flexible, heap-allocated data structure
 */
 
 pub fn run() {
+
+    // str data type inferred
+    let str_hello = "Hello";
+
+    // String data type explicit
+    let string_hello = String::from("hello!");
+
+    println!("{}, {} {strl} by {stringl}.", str_hello, string_hello, strl = str_hello.len(), stringl = string_hello.len());
 
 }
