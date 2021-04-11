f2de819ad7b39539810dc18ca00a59b5daa0d9ac
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 09:15:07 -0600

    Updated comments

--- src/strings.rs
+++ src/strings.rs
@@ -15,9 +15,11 @@ pub fn run() {
     */ 
 
     // String data type explicit
-    let mut string_hello = String::from("hello! ");
+    let mut string_hello = String::from("hello! "); // string_hello is set to `mut` to allow push and push_str to modify string_hello
 
-    string_hello.push('\u{1F600}'); // ...will succeed, when string_hello is set to `mut`. Can only ad hoc one `char`.
+    string_hello.push('\u{1F600}'); // push can only ad hoc one `char`.
+
+    string_hello.push_str(" \u{1F600} \u{1F600} \u{1F600} \u{1F600} \u{1F600} \u{1F600} \u{1F600}"); // push_str can add `char` and `&str`
 
     println!("{}{}\n{strl}:{stringl}", str_hello, string_hello, strl = str_hello.len(), stringl = string_hello.len());
 
