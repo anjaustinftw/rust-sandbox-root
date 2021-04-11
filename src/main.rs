0071626150aa8db9236c9ee2fd30e51ab8f89200
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-10 20:54:57 -0600

    Printing to the console and including external resources

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // Type 'cargo run' to build and run main.rs
 // Type './target\\debug\\sandbox' to run compiled executable
 
+// Required Files
+mod print;
+
 fn main() {
-    println!("Hello, world!");
+    print::run();
 }
---
+++ src/print.rs
@@ -0,0 +1,4 @@
+pub fn run() {
+    // Pring to console
+    println!("Hello world, from the print.rs file!");
+}
