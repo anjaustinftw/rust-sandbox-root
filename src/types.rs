3601c5fecf40123eb26f5e6bdd7a230aa8c19866
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 07:03:51 -0600

    Added primitive data type list to types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,28 @@
+// Data Types in Rust : https://doc.rust-lang.org/book/ch03-02-data-types.html#:~:text=Rust%20has%20two%20primitive%20compound%20types%3A%20tuples%20and%20arrays.
+// bool : The boolean type.
+// char : A character type.
+// int
+// i8 : The 8-bit signed integer type.
+// i16 : The 16-bit signed integer type.
+// i32 : The 32-bit signed integer type.
+// i64 : The 64-bit signed integer type.
+// isize : The pointer-sized signed integer type.
+// uint
+// u8 : The 8-bit unsigned integer type.
+// u16 : The 16-bit unsigned integer type.
+// u32 : The 32-bit unsigned integer type.
+// u64 : The 64-bit unsigned integer type.
+// usize : The pointer-sized unsigned integer type.
+// float
+// f32 : The 32-bit floating point type.
+// f64 : The 64-bit floating point type.
+// Compound Data Types
+// array : A fixed-size array, denoted [T; N], for the element type, T, and the non-negative compile-time constant size, N.
+// slice : A dynamically-sized view into a contiguous sequence, [T].
+// str : String slices.
+// tuple : A finite heterogeneous sequence, (T, U, ..).
+
 pub fn run() {
 
-    
+
 }
