18c34a89956c71c4b39e9f790434bfab48f050c5
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 20:33:57 -0600

    Start Structs

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@
 // mod conditionals;
 // mod loops;
 // mod functions;
-mod pointer_ref;
+// mod pointer_ref;
+mod structs;
 
 /* FRONTDOOR */
 fn main() {
@@ -29,6 +30,7 @@ fn main() {
     // conditionals::run();
     // loops::run();
     // functions::run();
-    pointer_ref::run();
+    // pointer_ref::run();
+    structs::run();
 
 }
