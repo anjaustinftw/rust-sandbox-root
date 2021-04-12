1c795a925ca8beb343fde6dcbc391a627b7aa726
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-11 20:03:11 -0600

    Functions End

--- src/functions.rs
+++ src/functions.rs
@@ -1,10 +1,29 @@
 /*
+    Functions are reusable blocks of block-scoped code.
 
+    Closures are not block-scoped, resuable segments of code.
 
 */
 
 pub fn run() {
 
+    greeting("Grettings,", "Jane");
 
+    let get_sum = add(5,5); // binding function values to variables
+    println!("sum = {}", get_sum);
 
+    let n3: i32 = 10;
+    let add_nums = |n1: i32, n2: i32| n1 + n2 + n3; // closures
+    println!("closure sum = {}", add_nums(4,4));
+
+}
+
+fn greeting(greet: &str, name: &str) {
+
+    println!("{} {}, nice to meet you!", greet, name);
+
+}
+
+fn add(n1: i32, n2: i32) -> i32 {
+    n1 + n2
 }
