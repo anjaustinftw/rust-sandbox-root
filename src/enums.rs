be607457b810567a17014f241c9da8400c1b53de
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-12 07:21:36 -0600

    Enums End

--- src/enums.rs
+++ src/enums.rs
@@ -12,17 +12,26 @@ enum Movement {
 }
 
 fn move_avatar(m: Movement) {
-    // Input-directed behavior
-    match m {
-        Movement::Up => println!("Avatar moving Up.");
-        Movement::Down => println!("Avatar moving Down.");
-        Movement::Right => println!("Avatar moving Right.");
-        Movement::Left => println!("Avatar moving Left.");
+    
+    match m {                                                       // Input-directed behavior options
+        Movement::Up    => println!("Avatar moving Up."),
+        Movement::Down  => println!("Avatar moving Down."),
+        Movement::Right => println!("Avatar moving Right."),
+        Movement::Left  => println!("Avatar moving Left.")
     }
+
 }
 
 pub fn run() {
 
+    let avtr_up     = Movement::Up;
+    let avtr_down   = Movement::Down;
+    let avtr_right  = Movement::Right;
+    let avtr_left   = Movement::Left;
 
+    move_avatar(avtr_up);                                           // Directional inputs triggered by signals from user interface
+    move_avatar(avtr_down);
+    move_avatar(avtr_right);
+    move_avatar(avtr_left);
 
 }
